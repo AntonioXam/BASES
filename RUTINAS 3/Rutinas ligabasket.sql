78e3426204d5cf65428eeacefdc72a72a7b5b9ef
AntonioXam <antoniojesus.ibanez.lopez@ces-vegamedia.es>
2024-05-28 02:18:14 +0200

    zxc

--- RUTINAS 3/Rutinas ligabasket.sql
+++ RUTINAS 3/Rutinas ligabasket.sql
@@ -7,8 +7,7 @@ CREATE DATABASE ligabasket;
 USE ligabasket;
 
 
-SOURCE C:\ligabasket.sql;
-
+SOURCE C:/ligabasket.sql
 --2
 
 describe equipos;
@@ -21,3 +20,138 @@ SELECT * FROM partidos;
 
 
 
++-----------+-----------+------+-----+---------+----------------+
+| Field     | Type      | Null | Key | Default | Extra          |
++-----------+-----------+------+-----+---------+----------------+
+| id_equipo | int(11)   | NO   | PRI | NULL    | auto_increment |
+| nombre    | char(50)  | NO   |     | NULL    |                |
+| ciudad    | char(50)  | NO   |     | NULL    |                |
+| pabellon  | char(100) | YES  |     | NULL    |                |
++-----------+-----------+------+-----+---------+----------------+
+
++------------+----------+------+-----+---------+----------------+
+| Field      | Type     | Null | Key | Default | Extra          |
++------------+----------+------+-----+---------+----------------+
+| id_jugador | int(11)  | NO   | PRI | NULL    | auto_increment |
+| nombre     | char(30) | NO   |     | NULL    |                |
+| apellido   | char(30) | NO   |     | NULL    |                |
+| puesto     | char(20) | NO   |     | NULL    |                |
+| salario    | int(11)  | NO   |     | NULL    |                |
+| altura     | float    | YES  |     | NULL    |                |
+| id_capitan | int(11)  | YES  | MUL | NULL    |                |
+| equipo     | int(11)  | NO   | MUL | NULL    |                |
++------------+----------+------+-----+---------+----------------+
+
++------------+---------+------+-----+---------+----------------+
+| Field      | Type    | Null | Key | Default | Extra          |
++------------+---------+------+-----+---------+----------------+
+| id_partido | int(11) | NO   | PRI | NULL    | auto_increment |
+| elocal     | int(11) | NO   | MUL | NULL    |                |
+| evisit     | int(11) | NO   | MUL | NULL    |                |
+| puntosL    | int(11) | NO   |     | NULL    |                |
+| puntosV    | int(11) | NO   |     | NULL    |                |
+| fecha      | date    | YES  |     | NULL    |                |
++------------+---------+------+-----+---------+----------------+
+
+
+
+
+#--Crea un procedimiento que muestre el nombre del equipo ganador de un partido. El procedimiento
+#--recibirá como parámetro el identificador del partido sin inner join 
+
+
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS ganador $$
+CREATE PROCEDURE ganador (IN p INT)
+BEGIN
+DECLARE resLocal, resVisit, vencedor INT;
+SET resLocal=(SELECT puntosL FROM Partidos WHERE id_partido=p);
+SET resVisit=(SELECT puntosV FROM Partidos WHERE id_partido=p);
+IF (resLocal>resVisit) THEN
+SET vencedor=(SELECT elocal FROM Partidos WHERE id_partido=p);
+ELSE
+SET vencedor=(SELECT evisit FROM Partidos WHERE id_partido=p);
+END IF;
+SELECT nombre AS Ganador
+FROM Equipos
+WHERE id_equipo=vencedor;
+END $$
+DELIMITER ;
+
+
+/*Crea una función que indique el número de partidos que ha ganado un equipo. La función recibirá
+como parámetro el identificador del equipo.*/
+
+DELIMITER $$
+DROP FUNCTION IF EXISTS partidos_ganados $$
+CREATE FUNCTION partidos_ganados (id_equipo INT) RETURNS INT
+BEGIN
+DECLARE num_ganados INT;
+SELECT COUNT(*)
+INTO num_ganados
+FROM Partidos
+WHERE (elocal = id_equipo AND puntosL > puntosV) OR (evisit = id_equipo AND puntosV > puntosL);
+RETURN num_ganados;
+END $$
+DELIMITER ;
+
+
+/*Crea una función que indique el total de puntos a favor (o anotados) de un equipo. La función recibirá
+como parámetro el identificador del equipo.sin usar case ni el into*/
+
+
+DELIMITER $$
+DROP FUNCTION IF EXISTS puntos_favor $$
+CREATE FUNCTION puntos_favor (e INT) RETURNS INT
+BEGIN
+DECLARE sumaL, sumaV INT;
+SET sumaL=(SELECT SUM(puntosL) FROM Partidos WHERE elocal=e);
+SET sumaV=(SELECT SUM(puntosV) FROM Partidos WHERE evisit=e);
+RETURN IFNULL(sumaL,0)+IFNULL(sumaV,0);
+END $$
+DELIMITER ;
+
+
+
+
+
+
+/*Crea una función que indique el total de puntos en contra (o recibidos) de un equipo. La función
+recibirá como parámetro el identificador del equipo.sin usar case ni el into*/
+
+
+
+DELIMITER $$
+DROP FUNCTION IF EXISTS puntos_contra $$
+CREATE FUNCTION puntos_contra (e INT) RETURNS INT
+BEGIN
+DECLARE recibidosL, recibidosV INT;
+SET recibidosL=(SELECT SUM(puntosV) FROM Partidos WHERE elocal=e);
+SET recibidosV=(SELECT SUM(puntosL) FROM Partidos WHERE evisit=e);
+RETURN IFNULL(recibidosL,0)+IFNULL(recibidosV,0);
+END $$
+DELIMITER ;
+
+/*Crea un procedimiento que reciba como parámetro el identificador de un equipo y muestre en una misma tabla la siguiente
+información: nombre del equipo, número de partidos ganados, número de partidos perdidos, total de
+puntos a favor y total de puntos en contra.*/
+
+
+
+
+
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS mostrar_informacion_equipo $$
+CREATE PROCEDURE mostrar_informacion_equipo (e INT)
+BEGIN
+SELECT nombre AS 'Nombre del Equipo',
+(SELECT COUNT(*) FROM Partidos WHERE elocal=e AND puntosL>puntosV) AS 'Partidos Ganados',
+(SELECT COUNT(*) FROM Partidos WHERE elocal=e AND puntosL<puntosV) AS 'Partidos Perdidos',
+puntos_favor(e) AS 'Total de Puntos a Favor',
+puntos_contra(e) AS 'Total de Puntos en Contra'
+FROM Equipos
+WHERE id_equipo=e;
+END $$
+DELIMITER ;
---
+++ RUTINAS 3/SP_SP.sql
@@ -0,0 +1,105 @@
+DROP DATABASE IF EXISTS Envios;
+
+CREATE DATABASE Envios;
+
+USE Envios;
+
+source C:/envios.sql
+
+SELECT * FROM S;
+SELECT * FROM P;
+SELECT * FROM SP;
+
+mysql> SELECT * FROM S;
++----+---------+--------+---------+
+| sn | snombre | estado | ciudad  |
++----+---------+--------+---------+
+| S1 | Salazar |     20 | Londres |
+| S2 | Jaimes  |     10 | Paris   |
+| S3 | Bernal  |     30 | Paris   |
+| S4 | Corona  |     20 | Londres |
+| S5 | Aldana  |   NULL | Atenas  |
++----+---------+--------+---------+
+5 rows in set (0.00 sec)
+
+mysql> SELECT * FROM P;
++----+---------+-------+------+---------+
+| pn | pnombre | color | peso | ciudad  |
++----+---------+-------+------+---------+
+| P1 | tuerca  | verde |   12 | Paris   |
+| P2 | perno   | rojo  |   17 | Londres |
+| P3 | birlo   | azul  |   17 | Roma    |
+| P4 | birlo   | rojo  |   14 | Londres |
+| P5 | leva    | azul  |   12 | Paris   |
+| P6 | engrane | rojo  |   19 | Paris   |
++----+---------+-------+------+---------+
+6 rows in set (0.00 sec)
+
+mysql> SELECT * FROM SP;
++----+----+------+
+| sn | pn | cant |
++----+----+------+
+| S1 | P1 |  300 |
+| S1 | P2 |  200 |
+| S1 | P3 |  400 |
+| S1 | P4 |  200 |
+| S1 | P5 |  100 |
+| S1 | P6 |  100 |
+| S2 | P1 |  300 |
+| S2 | P2 |  400 |
+| S3 | P2 |  200 |
+| S4 | P2 |  200 |
+| S4 | P3 |  100 |
+| S4 | P4 |  300 |
+| S4 | P5 |  400 |
++----+----+------+
+
+#--procedimiento que muestre el nombre de las piezas que ha enviado un proveedor 'sn'
+#-- el procedimiento recibirá como parámetro el codigo del proveedor 'sn' sin inner join con 
+
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS PiezasEnviadas $$
+
+CREATE PROCEDURE PiezasEnviadas(IN proveedor VARCHAR(4))
+BEGIN
+    SELECT P.pnombre
+    FROM P
+    WHERE P.pn IN (SELECT SP.pn FROM SP WHERE SP.sn = proveedor);
+END$$
+
+DELIMITER ;
+
+#-- Llamar al procedimiento con el código del proveedor 'S1'
+CALL PiezasEnviadas('S1');
+
+
+#--Funcion que determine el peso total de un Envio
+#--la funcion recibira como parametro el codigo del proveedor 'sn' y el codigo de la pieza 'pn' de la que hace el envio sin inner join
+
+DELIMITER $$
+
+
+DROP FUNCTION IF EXISTS PesoTotalEnvio $$
+
+CREATE FUNCTION PesoTotalEnvio(proveedor VARCHAR(4), pieza VARCHAR(4))
+RETURNS INT
+BEGIN
+    DECLARE peso_total INT;
+    
+    SELECT SUM(P.peso)
+    INTO peso_total
+    FROM P
+    WHERE P.pn = pieza AND P.pn IN (SELECT SP.pn FROM SP WHERE SP.sn = proveedor);
+    
+    RETURN peso_total;
+END$$
+
+DELIMITER ;
+
+#-- Llamar a la función con el código del proveedor 'S1' y el código de la pieza 'P1'
+SELECT PesoTotalEnvio('S1', 'P1');
+
+La función `PesoTotalEnvio` calcula el peso total de un envío realizado por un proveedor específico y de una pieza específica. En este caso, se llama a la función con el código del proveedor 'S1' y el código de la pieza 'P1'. La función sumará los pesos de todas las piezas de ese proveedor que coincidan con el código de la pieza 'P1' y devolverá el peso total del envío.
+
+Por favor, ten en cuenta que esta función asume la existencia de las tablas `P` y `SP`, así como sus respectivas columnas. Asegúrate de que las tablas y columnas existen y contienen los datos necesarios para que la función funcione correctamente.
